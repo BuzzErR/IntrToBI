9ab78d3e1fb29f704bc6c45fb45079c8b2858df7
BuzzErR <new.mikhailforwork@gmail.com>
2020-10-16 15:18:37 +0300

    orders completed

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import phrases
 from telebot import types
 import time
 
-logging.basicConfig(filename=config.log_file_name,level=logging.DEBUG)
+logging.basicConfig(filename=config.log_file_name, level=logging.DEBUG)
 bot = telebot.TeleBot(config.token, threaded=False)
 logging.getLogger("requests").setLevel(logging.WARNING)
 
@@ -77,7 +77,8 @@ try:
     def apply_pass(message):
         if functions.get_value_from_users(message, 'STATUS') == 'Waiting_for_passport':
             if functions.get_value_from_users(message, 'PASS_FILE') == '(None,)' or \
-                    functions.get_dif_of_time(functions.get_value_from_users(message, 'PASS_FILE').split('_')[1], datetime.datetime.now()) > 24:
+                    functions.get_dif_of_time(functions.get_value_from_users(message, 'PASS_FILE').split('_')[1],
+                                              datetime.datetime.now()) > 24:
                 file_id = message.photo[-1].file_id
                 file_info = bot.get_file(file_id)
                 downloaded_file = bot.download_file(file_info.file_path)
@@ -88,7 +89,8 @@ try:
                 functions.send_photo_to_admin(file_name, message, bot)
                 bot.send_message(message.chat.id, phrases.pass_photo_received)
             else:
-                estimated_time = 24 - functions.get_dif_of_time(functions.get_value_from_users(message, 'PASS_FILE').split('_')[1], datetime.datetime.now())
+                estimated_time = 24 - functions.get_dif_of_time(
+                    functions.get_value_from_users(message, 'PASS_FILE').split('_')[1], datetime.datetime.now())
                 bot.send_message(message.chat.id, phrases.wrong_time_of_photo + str(estimated_time) + ' часа')
 
 
@@ -171,8 +173,6 @@ try:
                         query = 'UPDATE tasks SET Courier=? WHERE id=?'
                         cursor.execute(query, [courier_id, task_id])
                         conn.commit()
-                        conn = sqlite3.connect('users.db')
-                        cursor = conn.cursor()
                         query = 'SELECT * FROM users WHERE Telegram_id=?'
                         courier = cursor.execute(query, [courier_id]).fetchone()
                         query = 'SELECT Owner_telegram_id FROM tasks WHERE id=?'
@@ -192,6 +192,56 @@ try:
                         bot.send_message(courier_id, 'Увы, данный заказ уже кто-то взял')
                 else:
                     bot.send_message(courier_id, 'У тебя ещё есть невыполненный заказ, новый ты пока взять не можешь')
+
+            elif data[2] == 'ACCEPT_PRICE':
+                conn = sqlite3.connect('users.db')
+                cursor = conn.cursor()
+                query = 'UPDATE tasks SET Total_real_price=? WHERE id=?'
+                cursor.execute(query, [data[1], data[0]])
+                conn.commit()
+                query = 'SELECT Owner_telegram_id FROM tasks WHERE id=?'
+                customer_id = cursor.execute(query, [data[0]]).fetchone()[0]
+                query = 'SELECT Courier FROM tasks WHERE id=?'
+                courier_id = cursor.execute(query, [data[0]]).fetchone()[0]
+                conn.close()
+                bot.send_message(customer_id, 'Отлично, скоро курьер доставит Вам продукты.')
+                keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=False)
+                button_phone = types.KeyboardButton(text="Я прибыл на место")
+                keyboard.add(button_phone)
+                bot.send_message(courier_id, 'Отправь геолокацию, как прибудешь на место, и заверши заказ', reply_markup=keyboard)
+            elif data[2] == 'DECLINE_PRICE':
+                conn = sqlite3.connect('users.db')
+                cursor = conn.cursor()
+                query = 'SELECT Owner_telegram_id FROM tasks WHERE id=?'
+                customer_id = cursor.execute(query, [data[0]]).fetchone()[0]
+                query = 'SELECT Courier FROM tasks WHERE id=?'
+                courier_id = cursor.execute(query, [data[0]]).fetchone()[0]
+                conn.close()
+                bot.send_message(customer_id, 'Проблема передана администратору, дождитесь ответа.')
+                bot.send_message(courier_id, 'Заказчик не подтвердил совпадение, проблема передана '
+                                             'администратору, дождитесь ответа.')
+            elif data[2] == 'COMPLETE_ORDER':
+                conn = sqlite3.connect('users.db')
+                cursor = conn.cursor()
+                query = 'SELECT * FROM tasks WHERE Owner_telegram_id=? AND Is_completed="FALSE"'
+                task_data = cursor.execute(query, [data[0]]).fetchone()
+                query = 'SELECT balance FROM users WHERE Telegram_id=?'
+                user_balance = cursor.execute(query, [task_data[1]]).fetchone()[0]
+                query = 'UPDATE users SET balance=? WHERE Telegram_id=?'
+                cursor.execute(query, [(task_data[13]-user_balance), task_data[1]])
+                conn.commit()
+                query = 'SELECT balance FROM users WHERE Telegram_id=?'
+                courier_balance = cursor.execute(query, [task_data[6]]).fetchone()[0]
+                query = 'UPDATE users SET balance=? WHERE Telegram_id=?'
+                cursor.execute(query, [(task_data[3] + courier_balance), task_data[6]])
+                conn.commit()
+                bot.send_message(task_data[1], 'Были рады иметь с Вами дело! \nМы обновили Ваш баланс в соответствии с '
+                                               'реальной ценой за продукты')
+                bot.send_message(task_data[6], 'На Ваш баланс зачислены деньги за доставку. Спасибо за то, что работаете с '
+                                               'нами.')
+                query = 'UPDATE tasks SET Is_completed="TRUE" WHERE id=?'
+                cursor.execute(query, [task_data[0]])
+                conn.close()
             else:
                 pass
 
@@ -211,11 +261,55 @@ try:
             bot.send_message(message.chat.id, 'Отлично, просто отправьте радиус в километрах '
                                               '(десятичную часть записывайте через точку)')
         elif message.text == 'Отправить фотографию товаров':
-            conn = sqlite3.connect('users.db')
             bot.send_message(message.chat.id, 'Жду')
             bot.register_next_step_handler(message, get_photo_of_goods)
+        elif message.text == 'Отправить фотографию чека':
+            bot.send_message(message.chat.id, 'Жду')
+            bot.register_next_step_handler(message, get_photo_of_bill)
+        elif message.text == 'Я прибыл на место':
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            query = 'SELECT Total_real_price FROM tasks WHERE Is_completed="FALSE" AND Courier=?'
+            price = cursor.execute(query, [message.from_user.id]).fetchone()
+            conn.close()
+            if price is not None:
+                bot.send_message(message.chat.id, 'Отправь геолокацию, чтобы завершить заказ, не забудьте связаться с '
+                                                  'заказчиком, чтобы уточнить где оставить продукты. Как только заказчик '
+                                                  'подтвердит получение - деньги будут переведены на Ваш счёт.')
+                bot.register_next_step_handler(message, get_final_location)
+            else:
+                bot.send_message(message.chat.id, 'Не совсем тебя понял.')
         else:
             bot.send_message(message.chat.id, phrases.unexpected_message)
+
+
+    def get_final_location(message):
+        try:
+            lat, lon = message.location.latitude, message.location.longitude
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            query = 'SELECT Home_lat, Home_long FROM tasks WHERE Courier=? AND Is_completed="FALSE"'
+            location = cursor.execute(query, [message.from_user.id]).fetchone()
+            conn.close()
+            if functions.calculate_distance(lat, lon, location[0], location[1]) < 0.05:
+                conn = sqlite3.connect('users.db')
+                cursor = conn.cursor()
+                query = 'SELECT Owner_telegram_id FROM tasks WHERE Courier=? AND Is_completed="FALSE"'
+                customer_id = cursor.execute(query, [message.from_user.id]).fetchone()[0]
+                conn.close()
+                markup = types.InlineKeyboardMarkup()
+                data = str(message.chat.id) + '-0'
+                btn1 = types.InlineKeyboardButton(text='Подтвердить\u2705', callback_data='{0}-COMPLETE_ORDER'.format(
+                    data))
+                markup.add(btn1)
+                bot.send_message(customer_id, 'Ваш заказ доставлен, подтвердите получение товаров.', reply_markup=markup)
+            else:
+                bot.send_message(message.chat.id, 'Как-то ты слишком далеко, подойди поближе')
+                bot.register_next_step_handler(message, get_final_location)
+        except Exception as e:
+            print(e)
+            bot.send_message(message.chat.id, phrases.wrong_location)
+            bot.register_next_step_handler(message, enter_point)
 
 
     def get_photo_of_goods(message):
@@ -224,12 +318,12 @@ try:
             file_info = bot.get_file(file_id)
             conn = sqlite3.connect('users.db')
             cursor = conn.cursor()
-            query = 'SELECT id FROM tasks WHERE Courier=?'
+            query = 'SELECT id FROM tasks WHERE Courier=? AND Is_completed="FALSE"'
             task_id = cursor.execute(query, [message.from_user.id]).fetchone()[0]
             cursor = conn.cursor()
             query = 'SELECT Photo_of_products FROM tasks Where id=?'
             photo = cursor.execute(query, [task_id]).fetchone()[0]
-            if photo == None:
+            if photo is None:
                 downloaded_file = bot.download_file(file_info.file_path)
                 file_name = 'GOODS_' + str(task_id) + '_' + str(datetime.datetime.now())
                 with open('goods/' + file_name, 'wb') as new_file:
@@ -243,13 +337,13 @@ try:
                 customer_id = cursor.execute(query, [task_id]).fetchone()[0]
                 bot.send_photo(customer_id, img)
                 bot.send_message(customer_id, 'Ваш заказ уже в пути, в скором времени перешлю фотографию чека.')
-                bot.register_next_step_handler(message, get_photo_of_bill)
                 query = 'UPDATE tasks SET Photo_of_products=? WHERE id=?'
                 cursor.execute(query, [file_name, task_id])
                 conn.commit()
                 conn.close()
             else:
                 bot.send_message(message.chat.id, 'Погоди, ты уже отправлял фотку товаров, так не пойдёт.')
+                conn.close()
         except Exception as e:
             bot.register_next_step_handler(message, get_photo_of_goods)
             bot.send_message(message.chat.id, 'Кажется, это не фоторафия, отправь ещё раз.')
@@ -261,35 +355,61 @@ try:
             file_info = bot.get_file(file_id)
             conn = sqlite3.connect('users.db')
             cursor = conn.cursor()
-            query = 'SELECT id FROM tasks WHERE Courier=?'
+            query = 'SELECT id FROM tasks WHERE Courier=? AND Is_completed="FALSE"'
             task_id = cursor.execute(query, [message.from_user.id]).fetchone()[0]
             cursor = conn.cursor()
-            query = 'SELECT Photo_of_products FROM tasks Where id=?'
+            query = 'SELECT Photo_of_bill FROM tasks Where id=?'
             photo = cursor.execute(query, [task_id]).fetchone()[0]
-            if photo == None:
+            if photo is None:
                 downloaded_file = bot.download_file(file_info.file_path)
-                file_name = 'GOODS_' + str(task_id) + '_' + str(datetime.datetime.now())
-                with open('goods/' + file_name, 'wb') as new_file:
+                file_name = 'BILL_' + str(task_id) + '_' + str(datetime.datetime.now())
+                with open('bills/' + file_name, 'wb') as new_file:
                     new_file.write(downloaded_file)
-                keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True, one_time_keyboard=True)
-                button_phone = types.KeyboardButton(text="Отправить фотографию чека")
-                keyboard.add(button_phone)
-                bot.send_message(message.chat.id, 'Принял, осталось прислать фотографию чека', reply_markup=keyboard)
-                img = open('goods/' + file_name, 'rb')
+                bot.send_message(message.chat.id, 'Сколько стоили товары? Пожалуйста, впиши точное число с чека,'
+                                                  ' заказчик будет должен подтвердить совпадение. Копейки '
+                                                  'указывай через точку')
+                img = open('bills/' + file_name, 'rb')
                 query = 'SELECT Owner_telegram_id FROM tasks WHERE id=?'
                 customer_id = cursor.execute(query, [task_id]).fetchone()[0]
                 bot.send_photo(customer_id, img)
-                bot.send_message(customer_id, 'Ваш заказ уже в пути, в скором времени перешлю фотографию чека.')
-                bot.register_next_step_handler(message, get_photo_of_bill)
-                query = 'UPDATE tasks SET Photo_of_products=? WHERE id=?'
+                bot.send_message(customer_id, 'Фотография чека прилагается.')
+                bot.register_next_step_handler(message, get_real_price_of_goods)
+                query = 'UPDATE tasks SET Photo_of_bill=? WHERE id=?'
                 cursor.execute(query, [file_name, task_id])
                 conn.commit()
                 conn.close()
             else:
-                bot.send_message(message.chat.id, 'Погоди, ты уже отправлял фотку товаров, так не пойдёт.')
+                bot.send_message(message.chat.id, 'Погоди, ты уже отправлял фотку чека, так не пойдёт.')
         except Exception as e:
-            bot.register_next_step_handler(message, get_photo_of_goods)
+            bot.register_next_step_handler(message, get_photo_of_bill)
             bot.send_message(message.chat.id, 'Кажется, это не фоторафия, отправь ещё раз.')
+
+
+    def get_real_price_of_goods(message):
+        try:
+            price = float(message.text)
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            query = 'SELECT id FROM tasks WHERE Courier=? AND Is_completed="FALSE"'
+            task_id = cursor.execute(query, [message.from_user.id]).fetchone()[0]
+            query = 'SELECT Owner_telegram_id FROM tasks WHERE id=?'
+            customer_id = cursor.execute(query, [task_id]).fetchone()[0]
+            message_text = 'Стоимость: ' + str(price) + '\n'
+            message_text += 'Пожалуйста, подтвердите совпадение данного числа с чеком.'
+            markup = types.InlineKeyboardMarkup()
+            data = str(task_id) + '-' + str(price)
+            btn1 = types.InlineKeyboardButton(text='\u2705', callback_data='{0}-ACCEPT_PRICE'.format(
+                data))
+            btn2 = types.InlineKeyboardButton(text='\uE333', callback_data='{0}-DECLINE_PRICE'.format(
+                data))
+            markup.add(btn1, btn2)
+            bot.send_message(customer_id, message_text, reply_markup=markup)
+            bot.send_message(message.chat.id, 'Пожалуйста, дождись, пока заказчик подтвердит, правильность данных')
+            conn.close()
+        except Exception as e:
+            print(e)
+            bot.register_next_step_handler(message, get_photo_of_goods)
+            bot.send_message(message.chat.id, 'Кажется, это не число, отправь ещё раз.')
 
 
     def set_courier_home_loc(message):
@@ -366,7 +486,7 @@ try:
             cursor.execute(query, value)
             conn.commit()
             conn.close()
-            bot.send_message(message.chat.id, 'Сколько будет стоить набор продуктов? Не волнуйся, если будет неточно, '
+            bot.send_message(message.chat.id, 'Сколько будет стоить набор товаров? Не волнуйтесь, если будет неточно, '
                                               'разница вернётся')
             bot.register_next_step_handler(message, set_price_for_goods)
         except Exception as e:
@@ -444,4 +564,5 @@ try:
 
 except Exception as e:
     logging.error(e)
+    print(e)
     time.sleep(3)
